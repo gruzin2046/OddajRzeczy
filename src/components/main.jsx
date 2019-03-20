5f1254c24a8c1268f32c0fba472a48353669b782
Albert Łuniewicz <albert2046@wp.pl>
2019-03-20 23:18:48 +0100

    adding files

---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './../css_files/Main.scss';
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <p>HELLO</p>
+            </div>
+        );
+    }
+}
