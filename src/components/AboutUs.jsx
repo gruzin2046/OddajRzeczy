d666b22db31142e0ecc81beab4412c062b807128
Albert Łuniewicz <albert2046@wp.pl>
2019-03-28 00:28:03 +0100

    about us updates

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,7 +5,14 @@ export default class AboutUs extends Component {
     render() {
         return (
             <div>
-                <p>HELLO</p>
+                <div className="aboutUs_container">
+                    <section className="aboutUs_section">
+                        <p>O nas</p>
+                        <div className="decoration_image"></div>
+                        <p className="abutUs_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat suscipit incidunt vero illo est modi voluptates repellat. Soluta in doloribus perspiciatis a, ex at officia ad, provident, iste illo illum.</p>
+                    </section>
+                    <div className="aboutUs_image"></div>
+                </div>
             </div>
         );
     }
