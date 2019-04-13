156ff8848410ba0ff141fb68384ba3dd7a3213d5
Albert Łuniewicz <albert2046@wp.pl>
2019-04-13 15:37:22 +0200

    helpDesc start

--- src/components/HelpDesc.jsx
+++ src/components/HelpDesc.jsx
@@ -4,9 +4,15 @@ import './../css_files/HelpDesc.scss';
 export default class FourSteps extends Component {
     render() {
         return (<div>
-            <section className="fourStepsSection">
+            <section className="helpStartSection">
                 <p>Komu pomagamy?</p>
                 <div className="decoration_image"></div>
+                <div className="helpOrganizations">
+                    <div className="organization">Fundacjom</div>
+                    <div className="organization">Organizacjom pozarządowym</div>
+                    <div className="organization">Lokalnym zbiórkom</div>
+                </div>
+                <div className="descriptionText">W naszej bazie znajdziesz listę zweryfikowanych fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</div>
             </section>
         </div>
         );
